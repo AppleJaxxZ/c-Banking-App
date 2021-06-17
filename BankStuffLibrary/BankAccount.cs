a5c6c227db95cd21f3ec44c10f5f79b85a2365e3
AppleJaxxZ <smackanalex@gmail.com>
2021-06-16 23:15:53 -0400

    Added another project class library to seperate bank related files to user related files

--- BankStuffLibrary/BankAccount.cs
+++ BankStuffLibrary/BankAccount.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace SuperBank
+namespace BankStuffLibrary
 
 {
     public class BankAccount
--- BankStuffLibrary/Transaction.cs
+++ BankStuffLibrary/Transaction.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace SuperBank
+namespace BankStuffLibrary
+
 
 {
     class Transaction
